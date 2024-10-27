a7ec3cd1f66026dde0050d0603aaac3c08bd3d0e
xiaowenchDS2023 <143538973+xiaowenchDS2023@users.noreply.github.com>
2024-10-27 02:25:10 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # 导入 CORS
 import joblib
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)  # 启用 CORS 支持
 
 # 加载模型
 random_forest_model = joblib.load("final_random_forest_model.joblib")
 voting_regressor_model = joblib.load("voting_regressor_model.joblib")
 
-# 根路径，检查服务是否运行
-@app.route('/')
-def home():
-    return "Football Bet Chatbot API is running"
-
-# 分类预测端点
 @app.route('/predict_classification', methods=['POST'])
 def predict_classification():
     data = request.get_json()
-    # 检查 'features' 是否存在
     if 'features' not in data:
         return jsonify({"error": "Missing 'features' in request"}), 400
     
@@ -25,11 +20,9 @@ def predict_classification():
     prediction = random_forest_model.predict(features)
     return jsonify({'classification_prediction': int(prediction[0])})
 
-# 回归预测端点
 @app.route('/predict_regression', methods=['POST'])
 def predict_regression():
     data = request.get_json()
-    # 检查 'features' 是否存在
     if 'features' not in data:
         return jsonify({"error": "Missing 'features' in request"}), 400
     
